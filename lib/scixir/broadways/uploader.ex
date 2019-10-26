70df0220c3d5c9d78f8b5bcbbf3c4fbed05f67f4
Phil Chen <06fahchen@gmail.com>
2019-10-26 20:59:34 +0800

    feat: add uploader

---
+++ lib/scixir/broadways/uploader.ex
@@ -0,0 +1,19 @@
+defmodule Scixir.Uploader do
+  alias Scixir.ScissorEvent
+
+  def upload(%ScissorEvent{bucket: bucket, key: key, version: version}, file_path) do
+    dirname  = Path.dirname(key)
+    extname  = Path.extname(key)
+    basename = Path.basename(key, extname)
+
+    versioned_key = Path.join([
+      dirname,
+      "#{basename}_#{version}#{extname}"
+    ])
+
+    file_path
+    |> ExAws.S3.Upload.stream_file()
+    |> ExAws.S3.upload(bucket, versioned_key, meta: [{"Scixir-Generated", "true"}])
+    |> ExAws.request()
+  end
+end
