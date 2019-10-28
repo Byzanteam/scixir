1775bc310cd1c53dbc0641b4ed0be0a500ae879a
Phil Chen <06fahchen@gmail.com>
2019-10-28 12:36:10 +0800

    feat: add SCIXIR_ prefix to config

--- config/dev.example.exs
+++ config/dev.example.exs
@@ -1,15 +1,15 @@
 import Config
 
 config :scixir,
-  redis_uri: System.get_env("MINIO_REDIS_URL"),
-  minio_broadway_list_name: System.get_env("MINIO_REDIS_NOTIFICATION_KEY"),
+  redis_uri: System.get_env("SCIXIR_MINIO_REDIS_URL"),
+  minio_broadway_list_name: System.get_env("SCIXIR_MINIO_BROADWAY_LIST_NAME"),
   scissor_broadway_list_name: System.get_env("SCIXIR_SCISSOR_BROADWAY_LIST_NAME")
 
 config :ex_aws,
-  access_key_id: System.get_env("MINIO_ACCESS_KEY"),
-  secret_access_key: System.get_env("MINIO_SECRET_KEY")
+  access_key_id: System.get_env("SCIXIR_MINIO_ACCESS_KEY"),
+  secret_access_key: System.get_env("SCIXIR_MINIO_SECRET_KEY")
 
 config :ex_aws, :s3,
-  scheme: System.fetch_env!("MINIO_SCHEME"),
-  host: System.get_env("MINIO_HOST"),
-  port: System.get_env("MINIO_PORT")
+  scheme: System.fetch_env!("SCIXIR_MINIO_SCHEME"),
+  host: System.get_env("SCIXIR_MINIO_HOST"),
+  port: System.get_env("SCIXIR_MINIO_PORT")
--- config/releases.exs
+++ config/releases.exs
@@ -1,18 +1,18 @@
 import Config
 
 config :scixir,
-  redis_uri: System.fetch_env!("MINIO_REDIS_URL"),
-  minio_broadway_list_name: System.fetch_env!("MINIO_REDIS_NOTIFICATION_KEY"),
+  redis_uri: System.fetch_env!("SCIXIR_MINIO_REDIS_URL"),
+  minio_broadway_list_name: System.fetch_env!("SCIXIR_MINIO_BROADWAY_LIST_NAME"),
   scissor_broadway_list_name: System.fetch_env!("SCIXIR_SCISSOR_BROADWAY_LIST_NAME")
 
 config :ex_aws,
-  access_key_id: System.fetch_env!("MINIO_ACCESS_KEY"),
-  secret_access_key: System.fetch_env!("MINIO_SECRET_KEY")
+  access_key_id: System.fetch_env!("SCIXIR_MINIO_ACCESS_KEY"),
+  secret_access_key: System.fetch_env!("SCIXIR_MINIO_SECRET_KEY")
 
 config :ex_aws, :s3,
-  scheme: System.fetch_env!("MINIO_SCHEME"),
-  host: System.fetch_env!("MINIO_HOST"),
-  port: System.fetch_env!("MINIO_PORT")
+  scheme: System.fetch_env!("SCIXIR_MINIO_SCHEME"),
+  host: System.fetch_env!("SCIXIR_MINIO_HOST"),
+  port: System.fetch_env!("SCIXIR_MINIO_PORT")
 
 logger_level =
   case System.get_env("MIX_LOGGER_LEVEL") do
