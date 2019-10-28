6bdf5a64d57a6b934cf79b998755fa01895075fd
Phil Chen <06fahchen@gmail.com>
2019-10-28 14:50:51 +0800

    bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Scixir.MixProject do
   def project do
     [
       app: :scixir,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.9.2",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
